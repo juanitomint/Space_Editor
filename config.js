ba175bf0c2f2683c3869a231f796255b10bdea40
Juan Ignacio Borda <juanignacioborda@gmail.com>
2013-11-13 17:25:46 -0300

    config file added for better env setup

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports ={
+    port:process.env.WEB_PORT || 3000,
+    appsDir:"/var/www/",
+    defaultTeamId:"dna2bpm",
+    google:{
+        returnUrl:'http://localhost:',
+    },
+    tree:{
+      showDotFolders : false,
+      showDotFiles : true
+    }
+};
