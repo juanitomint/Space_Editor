b4bea521003596d11c62c489686b2d47fd271bf3
Juan Borda <juanb@mp.gba.gov.ar>
2013-11-26 13:16:51 -0300

    removed logs

--- public/app/app.js
+++ public/app/app.js
@@ -14,26 +14,6 @@ Ext.application({
     setToolbarSettings: function(me) {
         if (me) {
             Ext.getCmp('EditorToolbar').setDisabled(false);
-            /*
-             //----clear all
-             Ext.getCmp('CodeFolding').setValue(false);
-             Ext.getCmp('WrapLines').setValue(false);
-             Ext.getCmp('HighlightActiveLine').setValue(false);
-             Ext.getCmp('ShowInvisibles').setValue(false); 
-            
-             console.log('-----------------------------------------------');
-             console.log('toolbar for file:', me.path);
-             console.log('-----------------------------------------------');
-             console.log('codeFolding', me.codeFolding);
-             console.log('WrapLines', me.useWrapMode);
-             console.log('HighlightActiveLine', me.highlightActiveLine);
-             console.log('ShowInvisibles', me.showInvisible);
-             console.log('FontSize', me.fontSize);
-             console.log('EditorTheme', me.theme);
-             console.log('Object', me);
-             console.log('-----------------------------------------------');
-             */
-
             Ext.getCmp('CodeFolding').setValue(me.codeFolding);
             Ext.getCmp('WrapLines').setValue(me.useWrapMode);
             Ext.getCmp('HighlightActiveLine').setValue(me.highlightActiveLine);
